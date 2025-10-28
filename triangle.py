d8b83f2abb4f5fb09cd770425237070552fe4884
yanni <thbldprnce@gmail.com>
2025-10-28 03:55:59 +0300

    added triangle.py

---
+++ triangle.py
@@ -0,0 +1,27 @@
+def area(a, h):
+    """
+    Calculate the area of a triangle.
+
+    Parameters:
+    a (int): base of the triangle
+    h (int): height of the triangle
+
+    Returns:
+    float: area of the triangle calculated as (a * h) / 2
+    """
+    return a * h / 2
+
+
+def perimeter(a, b, c):
+    """
+    Calculate the perimeter of a triangle.
+
+    Parameters:
+    a (int): first side of the triangle
+    b (int): second side of the triangle
+    c (int): third side of the triangle
+
+    Returns:
+    int: perimeter of the triangle calculated as a + b + c
+    """
+    return a + b + c
